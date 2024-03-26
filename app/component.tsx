b450f6ee0ae545909268ffb28b1267ee46c9e387
bianxingming <xingming.bian@in2iot.com>
2024-03-26 17:40:29 +0800

    fix input valueAsNumber error

--- app/component.tsx
+++ app/component.tsx
@@ -221,7 +221,7 @@ const GroupBoard = ({
               <span className="text-gray-500 sm:text-sm">$</span>
             </div>
             <input
-              type="text"
+              type="number"
               name="price"
               id="price"
               className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
